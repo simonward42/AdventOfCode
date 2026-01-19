2837facba3155c9d2c302937a104d0f2902a98a5
Simon Ward <simon.ward@barnett-waddingham.co.uk>
2026-01-19 16:44:36 +0000

    D4 p1 solved

---
+++ 2025/AoC25.Tests/Day4/SolutionTests.cs
@@ -0,0 +1,65 @@
+namespace AoC25.Tests.Day4;
+
+using AoC25.Day4;
+
+using Shared.Util;
+
+public class SolutionTests
+{
+	Solution _sut;
+	IInputReader _realInput = new InputFileReader(@"Day4/realInput.txt");
+
+	[SetUp]
+	public void Setup()
+	{
+		_sut = new Solution();
+	}
+
+	[TearDown]
+	public void TearDown()
+	{
+		_sut.Dispose();
+	}
+
+	[OneTimeTearDown]
+	public void OneTimeTearDown()
+	{
+		_realInput.Dispose();
+	}
+
+	[Test]
+	public void TestPart1()
+	{
+		var expectedAnswer = 13;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	[Test]
+	public void TestPart1_RealInput()
+	{
+		_sut = new Solution(_realInput);
+
+		var expectedAnswer = 1537;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	//[Test]
+	//public void TestPart2()
+	//{
+	//	var expectedAnswer = 3121910778619;
+	//	var actualAnswer = _sut.GetPart2Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+
+	//[Test]
+	//public void TestPart2_RealInput()
+	//{
+	//	_sut = new Solution(_realInput);
+
+	//	var expectedAnswer = 171435596092638;
+	//	var actualAnswer = _sut.GetPart2Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+}
---
+++ 2025/AoC25/Day4/Solution.cs
@@ -0,0 +1,63 @@
+﻿namespace AoC25.Day4;
+
+using Shared.Util;
+
+public class Solution(IInputReader? reader = null) : Puzzle<int>(4, reader)
+{
+	//count the number of paper rolls ('@') in the input with fewer than 4 rolls in the eight adjacent positions
+	protected override int SolvePart1()
+	{
+		var rows = InputReader.ReadUntilEmptyLine();
+		var accessibleRollsCount = 0;
+
+		string row;
+		for (var y = 0; y < rows.Length; y++)
+		{
+			row = rows[y];
+			for (var x = 0; x < row.Length; x++)
+			{
+				if (row[x] == '@' && _AdjacentRollsCount(rows, x, y, row.Length) < 4)
+				{
+					accessibleRollsCount++;
+				}
+			}
+		}
+
+		return accessibleRollsCount;
+	}
+
+	private int _AdjacentRollsCount(string[] rows, int x, int y, int rowLength)
+	{
+		var rollsCount = 0;
+		for (int j = y - 1; j <= y + 1; j++)
+		{
+			//handle edges
+			if (j < 0 || j >= rows.Length) continue;
+
+			for (int i = x - 1; i <= x + 1; i++)
+			{
+				//don't self-count
+				if (i == x && j == y) continue;
+
+				//handle edges
+				if (i < 0 || i >= rowLength) continue;
+
+				if (rows[j][i] == '@')
+					rollsCount++;
+			}
+		}
+
+		return rollsCount;
+	}
+
+	//description
+	protected override int SolvePart2()
+	{
+		while (InputReader.TryReadLine(out string? currentLine))
+		{
+
+		}
+
+		return 0;
+	}
+}
