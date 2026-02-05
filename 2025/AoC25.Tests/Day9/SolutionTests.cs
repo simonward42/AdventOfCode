3fb4d47b937af9975373688bf9c96a17bcc6c366
Simon Ward <simon.ward@barnett-waddingham.co.uk>
2026-02-05 18:04:45 +0000

    D9 p1 solved

---
+++ 2025/AoC25.Tests/Day9/SolutionTests.cs
@@ -0,0 +1,67 @@
+namespace AoC25.Tests.Day9;
+
+using AoC25.Day9;
+
+using Shared.Util;
+
+public class SolutionTests
+{
+	Solution _sut;
+	IInputReader _realInput = new InputFileReader(@"Day9/realInput.txt");
+
+	[SetUp]
+	public void Setup()
+	{
+		_sut = new Solution();
+	}
+
+	[TearDown]
+	public void TearDown()
+	{
+		_sut.Dispose();
+	}
+
+	[OneTimeTearDown]
+	public void OneTimeTearDown()
+	{
+		_realInput.Dispose();
+	}
+
+	[Test]
+	public void TestPart1()
+	{
+		var expectedAnswer = 50;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	[Test]
+	public void TestPart1_RealInput()
+	{
+		_sut = new Solution(_realInput);
+
+		var expectedAnswer = 4741451444;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	//[Test]
+	//public void TestPart2()
+	//{
+	//	_sut.Verbose = true;
+	//	var expectedAnswer = 25272;
+	//	var actualAnswer = _sut.GetPart2Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+
+	//[Test]
+	//public void TestPart2_RealInput()
+	//{
+	//	_sut = new Solution(_realInput);
+	//	_sut.Verbose = true;
+
+	//	var expectedAnswer = 100011612;
+	//	var actualAnswer = _sut.GetPart2Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+}
---
+++ 2025/AoC25/Day9/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC25.Day9;
+
+using Shared.Util;
+
+public class Solution(IInputReader? reader = null) : Puzzle<long>(9, reader)
+{
+	//using the input 2D coordinates, find the rectangle of greatest area that can 
+	//be formed using any pair of input points as opposite corners. 
+	protected override long SolvePart1()
+	{
+		List<Point2d> tilePositions = [];
+		while (InputReader.TryReadLine(out string? line))
+		{
+			tilePositions.Add(new Point2d(line));
+		}
+
+		var pairAreas = tilePositions.CalculatePairwiseMetric((a, b) =>
+		{
+			//tiles have size 1x1
+			long width = Math.Abs(a.X - b.X) + 1;
+			long height = Math.Abs(a.Y - b.Y) + 1;
+			return width * height;
+		});
+
+		return pairAreas.Max(x => x.Value);
+	}
+
+	//description
+	protected override long SolvePart2()
+	{
+		while (InputReader.TryReadLine(out string? currentLine))
+		{
+
+		}
+
+		return 0;
+	}
+}
