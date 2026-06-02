56d210b21f9188b30397aa0c9d31ab8e8bcf93f1
Simon Ward <simon.ward@barnett-waddingham.co.uk>
2026-06-02 16:07:56 +0100

    Day10 utils: CartesianProduct of IEnumerables
    Will be used to construct all possible button presses

---
+++ 2025/AoC25.Tests/Day10/SolutionTests.cs
@@ -0,0 +1,140 @@
+namespace AoC25.Tests.Day10;
+
+using AoC25.Day10;
+
+using Shared.Util;
+
+public class SolutionTests
+{
+	Solution _sut;
+	IInputReader _realInput = new InputFileReader(@"Day10/realInput.txt");
+
+	[SetUp]
+	public void Setup()
+	{
+		_sut = new Solution();
+	}
+
+	[TearDown]
+	public void TearDown()
+	{
+		_sut.Dispose();
+	}
+
+	[OneTimeTearDown]
+	public void OneTimeTearDown()
+	{
+		_realInput.Dispose();
+	}
+
+	[Test]
+	public void Repeat()
+	{
+		var repeated = Enumerable.Repeat<List<int>>([0, 1], 3);
+
+		int[] range = [0, 1];
+		var expected = new[] {
+			range,
+			range,
+			range,
+		};
+
+		Assert.That(repeated, Is.EquivalentTo(expected));
+	}
+
+	[Test]
+	public void CartesianProduct()
+	{
+		//# product('ABCD', 'xy') → Ax Ay Bx By Cx Cy Dx Dy
+		//# product(range(2), repeat=3) → 000 001 010 011 100 101 110 111
+
+		char[] abcd = ['A', 'B', 'C'];
+		char[] xy = ['x', 'y'];
+		IEnumerable<IEnumerable<char>> sequences = [abcd, xy];
+
+		IEnumerable<IEnumerable<char>> expectedProduct = [['A', 'x'], ['A', 'y'], ['B', 'x'], ['B', 'y'], ['C', 'x'], ['C', 'y']];
+		Assert.That(sequences.CartesianProduct(), Is.EqualTo(expectedProduct));
+
+		//---
+
+		int[] bit = [0, 1];
+		var threeBits = Enumerable.Repeat(bit, 3);
+
+		IEnumerable<IEnumerable<int>> expected = [
+			[0, 0, 0],
+			[0, 0, 1],
+			[0, 1, 0],
+			[0, 1, 1],
+			[1, 0, 0],
+			[1, 0, 1],
+			[1, 1, 0],
+			[1, 1, 1]];
+
+		Assert.That(threeBits.CartesianProduct(), Is.EqualTo(expected));
+	}
+
+	[Test]
+	public void TestPart1_FirstLine()
+	{
+		_sut = new Solution(new InputStringReader("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"));
+		var expectedAnswer = 2;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	[Test]
+	public void TestPart1_SecondLine()
+	{
+		_sut = new Solution(new InputStringReader("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"));
+		var expectedAnswer = 3;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	[Test]
+	public void TestPart1_ThirdLine()
+	{
+		_sut = new Solution(new InputStringReader("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"));
+		var expectedAnswer = 2;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	[Test]
+	public void TestPart1()
+	{
+		var expectedAnswer = 7;
+		var actualAnswer = _sut.GetPart1Answer();
+		Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	}
+
+	//[Test]
+	//public void TestPart1_RealInput()
+	//{
+	//	_sut = new Solution(_realInput);
+
+	//	var expectedAnswer = 4741451444;
+	//	var actualAnswer = _sut.GetPart1Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+
+	//[Test]
+	//public void TestPart2()
+	//{
+	//	var expectedAnswer = 24;
+	//	var actualAnswer = _sut.GetPart2Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+
+	//[Test]
+	//[CancelAfter(0)]
+	//[Explicit] // takes ~1min to execute
+	//public void TestPart2_RealInput()
+	//{
+	//	_sut = new Solution(_realInput);
+
+	//	var expectedAnswer = 1562459680;
+	//	var actualAnswer = _sut.GetPart2Answer();
+	//	Assert.That(actualAnswer, Is.EqualTo(expectedAnswer));
+	//}
+}
--- Shared/Util/CollectionExtensions.cs
+++ Shared/Util/CollectionExtensions.cs
@@ -2,21 +2,33 @@
 
 public static class CollectionExtensions
 {
-    public static Dictionary<(T, T), M> CalculatePairwiseMetric<T, M>(this ICollection<T> collection, Func<T, T, M> metricFunc)
-    {
-        var metricValues = new Dictionary<(T, T), M>();
+	public static Dictionary<(T, T), M> CalculatePairwiseMetric<T, M>(this ICollection<T> collection, Func<T, T, M> metricFunc)
+	{
+		var metricValues = new Dictionary<(T, T), M>();
 
-        var exclude = new List<T>();
-        foreach (var item1 in collection)
-        {
-            exclude.Add(item1);
-            foreach (var item2 in collection.Except(exclude))
-            {
-                var metric = metricFunc(item1, item2);
-                metricValues.Add((item1, item2), metric);
-            }
-        }
+		var exclude = new List<T>();
+		foreach (var item1 in collection)
+		{
+			exclude.Add(item1);
+			foreach (var item2 in collection.Except(exclude))
+			{
+				var metric = metricFunc(item1, item2);
+				metricValues.Add((item1, item2), metric);
+			}
+		}
 
-        return metricValues;
-    }
+		return metricValues;
+	}
+
+	public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+	{
+		IEnumerable<IEnumerable<T>> emptyProduct = [[]];
+
+		return sequences.Aggregate(
+			emptyProduct,
+			(accumulator, sequence) =>
+				from acc in accumulator
+				from item in sequence
+				select acc.Concat([item]));
+	}
 }
